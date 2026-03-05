4310d19d1d5511bde87d1ea5ec12bb2654c34c61
Hireophant <tntdanh2506@gmail.com>
2026-03-06 03:50:16 +0700

    sort by key and value

--- controller/task.controller.ts
+++ controller/task.controller.ts
@@ -2,13 +2,24 @@ import Task from "../models/task.model";
 import { Request, Response } from "express";
 
 export const index = async (req: Request, res: Response) => {
+    //Find
     const find = {
         deleted: false,
     };
     if(req.query.status){
         find["status"] = req.query.status;
     }
-    const tasks = await Task.find(find);
+    //End find
+
+    //Sort
+    const sort = {}
+    if (req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey.toLocaleString();
+        sort[sortKey] = req.query.sortValue;
+    }
+    //End sort
+
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks);
 };
 
