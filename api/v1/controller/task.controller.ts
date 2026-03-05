936d55c86a6396ad3f4664d5054eda65b272c76e
Hireophant <tntdanh2506@gmail.com>
2026-03-06 04:39:34 +0700

    change multi status

--- api/v1/controller/task.controller.ts
+++ api/v1/controller/task.controller.ts
@@ -87,3 +87,48 @@ export const changeStatus = async (req: Request, res: Response) => {
         );
     }
 }
+
+// [PATCH] /tasks/change-multi
+export const changeMulti = async (req: Request, res: Response) => {
+    try {
+        const ids: string[] = req.body.ids;
+        const key: string = req.body.key;
+        const value: string = req.body.value;
+
+        switch (key) {
+            case "status":
+                const tasks = await Task.updateMany(
+                    {
+                        _id: {
+                            $in: ids,
+                        },
+                    },
+                    {
+                        status: value,
+                    }
+                );
+                res.json(
+                    {
+                        code: 200,
+                        message: "Change status success",
+                    }
+                );
+                break;
+            default:
+                res.json(
+                    {
+                        code: 400,
+                        message: "Không tồn tại",
+                    }
+                );
+                break;
+        }
+    }catch(error){
+        res.json(
+            {
+                code: 400,
+                message: "Change status failed",
+            }
+        );
+    }
+}
